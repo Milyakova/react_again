5ec19ceee5daf0532145e6118fa7782590a352b1
Milyakova <versuchi@yandex.ru>
2022-04-27 16:59:29 +0300

    http changed on httpS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const AUTHORS = {
 
 export const apiUrl = "https://api.spaceflightnewsapi.net/v3/articles";
 
-export const apiActivityUrl = "http://www.boredapi.com/api/activity/";
+export const apiActivityUrl = "https://www.boredapi.com/api/activity";
 
 export const FETCH_STATUSES = {
   IDLE: "idle",
